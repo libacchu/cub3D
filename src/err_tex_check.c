2ddf1eef59cc211bfbed1dd4f87eb34677a32113
Oliver Bibby <obibby@c2s19d128.42wolfsburg.de>
2023-01-11 11:28:13 +0100

    norm conforming err_tex_check

--- src/err_tex_check.c
+++ src/err_tex_check.c
@@ -1,38 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   err_tex_check.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obibby <obibby@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/11 11:27:27 by obibby            #+#    #+#             */
+/*   Updated: 2023/01/11 11:27:42 by obibby           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/cub3D.h"
 
-int check_tex(char *str, t_cub3D *game)
+int	alloc_wall(char *str, void *wall_ptr)
+{
+	if (wall_ptr)
+		return (err_message("Too many textures in mapfile."));
+	wall_ptr = ft_strdup(str);
+	if (!wall_ptr)
+		return (err_message("Memory allocation failed."));
+	return (0);
+}
+
+int	check_tex(char *str, t_cub3D *game)
 {
 	if (str[0] == 'N' && str[1] == 'O' && str[2] == ' ' && str[3])
 	{
-		if (game->north_wall)
-			return (err_message("Too many textures in mapfile."));
-		game->north_wall = ft_strdup(&str[3]);
-		if (!game->north_wall)
-			return (err_message("Memory allocation failed."));
+		if (alloc_wall(&str[3], game->north_wall))
+			return (1);
 	}
 	else if (str[0] == 'S' && str[1] == 'O' && str[2] == ' ' && str[3])
 	{
-		if (game->south_wall)
-			return (err_message("Too many textures in mapfile."));
-		game->south_wall = ft_strdup(&str[3]);
-		if (!game->south_wall)
-			return (err_message("Memory allocation failed."));
+		if (alloc_wall(&str[3], game->south_wall))
+			return (1);
 	}
 	else if (str[0] == 'W' && str[1] == 'E' && str[2] == ' ' && str[3])
 	{
-		if (game->west_wall)
-			return (err_message("Too many textures in mapfile."));
-		game->west_wall = ft_strdup(&str[3]);
-		if (!game->west_wall)
-			return (err_message("Memory allocation failed."));
+		if (alloc_wall(&str[3], game->west_wall))
+			return (1);
 	}
 	else if (str[0] == 'E' && str[1] == 'A' && str[2] == ' ' && str[3])
 	{
-		if (game->east_wall)
-			return (err_message("Too many textures in mapfile."));
-		game->east_wall = ft_strdup(&str[3]);
-		if (!game->east_wall)
-			return (err_message("Memory allocation failed."));
+		if (alloc_wall(&str[3], game->east_wall))
+			return (1);
 	}
 	else
 		return (1);
