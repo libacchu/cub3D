616bc5d9d0ef84d88ffe373132d291d39c47f160
Oliver Bibby <obibby@c2s19d128.42wolfsburg.de>
2023-01-11 11:02:39 +0100

    norm conforming err_file_check

--- src/err_file_check.c
+++ src/err_file_check.c
@@ -3,22 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   err_file_check.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
+/*   By: obibby <obibby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 10:43:11 by libacchu          #+#    #+#             */
-/*   Updated: 2023/01/11 10:43:18 by libacchu         ###   ########.fr       */
+/*   Updated: 2023/01/11 10:58:56 by obibby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3D.h"
 
+int	map_checks(t_cub3D *game, char *str, int fd, int i)
+{
+	if (i && !str)
+		return (err_message("No map."));
+	else if (i == 6 && check_map(str, game, fd))
+		return (1);
+	if (i == 0)
+		return (err_message("Empty file."));
+	if (i < 6)
+		return (err_message("Invalid map file."));
+}
+
+int	premap_check(t_cub3D *game, char *str)
+{
+	if (str[0] == 'F' || str[0] == 'C')
+	{
+		if (check_rgb(str, game))
+			return (err_message("Invalid colour element in map file."));
+	}
+	else if (!ft_strncmp(str, "NO ", 3) || !ft_strncmp(str, "SO ", 3)
+		|| !ft_strncmp(str, "EA ", 3) || !ft_strncmp(str, "WE ", 3))
+	{
+		if (check_tex(str, game))
+			return (1);
+	}
+	else
+		return (err_message("Invalid or missing element in map file."));
+}
+
 int	err_map(int fd, t_cub3D *game)
 {
-	char *str;
-	int i;
-	int flag;
-	
-	flag = 0;
+	char	*str;
+	int		i;
+
 	i = 0;
 	str = get_next_line(fd);
 	while (str && i != 6)
@@ -31,31 +58,14 @@ int	err_map(int fd, t_cub3D *game)
 		}
 		else
 		{
-			ft_printf(str);
-			if (str[0] == 'F' || str[0] == 'C')
-			{
-				if (check_rgb(str, game))
-					return (err_message("Invalid colour variable in map file."));
-			}
-			else if (!ft_strncmp(str, "NO ", 3) || !ft_strncmp(str, "SO ", 3) || !ft_strncmp(str, "EA ", 3) || !ft_strncmp(str, "WE ", 3) )
-			{
-				if (check_tex(str, game))
-					return (1);
-			}
-			else
-				return (err_message("Invalid or missing variable in map file."));
+			if (premap_check(game, str))
+				return (1);
 			i++;
 		}
 		free(str);
 		str = get_next_line(fd);
 	}
-	if (i && !str)
-		return (err_message("No map."));
-	else if (i == 6 && check_map(str, game, fd))
+	if (map_checks(game, str, fd, i))
 		return (1);
-	if (i == 0 )
-		return (err_message("Empty file."));
-	if (i < 6)
-		return (err_message("Invalid map file."));
 	return (0);
 }
