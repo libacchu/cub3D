a16566f54c8dc473e24489d6ba7462eb0b86ce00
Lincoln Bacchus <lincolnbacchus@gmail.com>
2023-01-18 23:14:28 +0100

    update header for norm

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: libacchu <libacchu@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 20:15:28 by libacchu          #+#    #+#             */
-/*   Updated: 2023/01/18 20:56:32 by libacchu         ###   ########.fr       */
+/*   Updated: 2023/01/18 23:12:38 by libacchu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,20 +45,20 @@ enum	e_keystroke
 
 typedef struct s_ray
 {
-	double	cameraX; // 2 * x / double(w) - 1;
-	double	rayDirX; // dirX + planeX * cameraX
-	double	rayDirY; // dirY + planeY * cameraX;
-	double	sideDistX; // dist to next X block.
-	double	sideDistY; // dist to next Y block.
-	double	deltaDistX; // dist to X block after next.
-	double	deltaDistY; // dist to Y block after next.
+	double	cameraX;
+	double	rayDirX;
+	double	rayDirY;
+	double	sideDistX;
+	double	sideDistY;
+	double	deltaDistX;
+	double	deltaDistY;
 	double	perpWallDist;
 	double	*zbuffer;
 	int		lineHeight;
-	int		drawStart; // height to begin drawing.
-	int		drawEnd; // height to finish drawing.
-	int		stepX; // direction of the next hit block.
-	int		stepY; // direction of the next hit block.
+	int		drawStart;
+	int		drawEnd;
+	int		stepX;
+	int		stepY;
 	double	tex_pos;
 	double	step;
 	int		mapX;
@@ -73,7 +73,7 @@ typedef struct s_ray
 	int		south_y;
 	int		west_x;
 	int		west_y;
-} t_ray;
+}	t_ray;
 
 typedef struct s_line
 {
@@ -87,7 +87,7 @@ typedef struct s_line
 	int		y0;
 	double	x1;
 	double	y1;
-} t_line;
+}	t_line;
 
 typedef struct s_door
 {
@@ -95,7 +95,7 @@ typedef struct s_door
 	int		y;
 	int		open;
 	void	*next;
-} t_door;
+}	t_door;
 
 typedef struct s_image
 {
@@ -104,9 +104,9 @@ typedef struct s_image
 	int		bpp;
 	int		line_size;
 	int		endian;
-} t_image;
+}	t_image;
 
-typedef	struct s_sprite
+typedef struct s_sprite
 {
 	double	x;
 	double	y;
@@ -118,7 +118,7 @@ typedef	struct s_sprite
 	int		sprite_scare;
 	int		sp_img_total[4];
 	void	*next;
-} t_sprite;
+}	t_sprite;
 
 typedef struct s_spr_cast
 {
@@ -131,16 +131,16 @@ typedef struct s_spr_cast
 	int		draw_start_x;
 	int		draw_end_x;
 	int		index;
-} t_spr_cast;
+}	t_spr_cast;
 
 typedef struct s_player
 {
-	char	direct; // N, S, E, W
-	double	posX;	// //x and y start position
+	char	direct;
+	double	posX;
 	double	posY;
-	double	dirX;	//initial direction vector
+	double	dirX;
 	double	dirY;
-	double	viewX;	//the 2d raycaster version of camera plane
+	double	viewX;
 	double	viewY;
 }	t_player;
 
@@ -178,12 +178,10 @@ typedef struct s_cub3D
 	t_sprite	*sprite_list;
 	t_sprite	**sprite_arr;
 	t_spr_cast	sprite_info;
-	t_image 	*sprite;
+	t_image		*sprite;
 	int			sprite_total;
 	int			*sprite_order;
 	int			*sprite_distance;
-
-
 	char		*path;
 	char		**map_arr;
 
@@ -210,108 +208,167 @@ typedef struct s_cub3D
 
 }	t_cub3D;
 
-int	render(t_cub3D *game);
-
-void	init_game(t_cub3D *game);
-int		err_map(int fd, t_cub3D *game);
-int		count_rows(char *path);
-int		create_map_arr(char *str, t_cub3D *game, int fd);
-int		check_chars(char c);
-int		check_map(char *str, t_cub3D *game, int fd);
-int		check_rgb(char *str, t_cub3D *game);
-int		check_tex(char *str, t_cub3D *game);
-void	set_screen_size(t_cub3D *game);
+/* assign_images */
+int		assign_compass(t_cub3D *game);
+int		assign_minimap(t_cub3D *game);
+int		assign_walls_ns(t_cub3D *game);
+int		assign_walls_ew(t_cub3D *game);
 int		assign_images(t_cub3D *game);
 
+/* door.c */
+int		add_door(t_cub3D *game, int x, int y);
+int		assign_door(t_cub3D *game);
+int		check_door(t_cub3D *game, int side);
+void	get_door_colour(t_cub3D *game, t_image *img, int x, int y);
+void	find_door(t_cub3D *game);
+
+/* door2.c */
+int		find_door_node(t_cub3D *game, int new_x, int new_y);
+void	open_door(t_cub3D *game, int x, int y);
+
+/* game_init.c */
+void	set_screen_size(t_cub3D *game);
+
+/* err_file_check.c */
+int		map_checks(t_cub3D *game, char *str, int fd, int i);
+int		premap_check(t_cub3D *game, char *str);
+int		err_map(int fd, t_cub3D *game);
+
+/* err_map_check.c */
 int		check_x(int c);
 int		check_y(char **map_arr, int y, int x);
 int		check_walls_chars(t_cub3D *game, int x, int y);
 int		loop_x(t_cub3D *game, int x, int y);
+int		check_map(char *str, t_cub3D *game, int fd);
 
+/* err_map_check2.c */
+int		count_rows(char *path);
+int		add_to_arr(char **str, t_cub3D *game, int fd);
+int		create_map_arr(char *str, t_cub3D *game, int fd);
+int		check_chars(char c);
+
+/* err_rgb_check.c */
+int		colourshift(int t, int r, int g, int b);
+int		convert_number(char *str, int *i, int *n);
+int		set_rgb(char *str, int *colour);
+int		check_rgb(char *str, t_cub3D *game);
+
+/* err_tex_check.c */
+int		alloc_wall(char *str, void **wall_ptr);
+int		check_tex(char *str, t_cub3D *game);
+
+/* error_check.c */
 int		err_message(char *str);
 int		err_path_name(char *path);
 int		only_white_space(char *str);
 int		errorcheck(char **argv, t_cub3D *game);
 
-double	ft_abs(double x);
-void	my_mlx_pixel_put(t_image *data, int x, int y, int color);
-void	ft_swap(int *distance_x, int *distance_y, int *order_x, int *order_y);
-
+/* exit.c */
 void	ft_exit(char *str, t_cub3D *game);
+
+/* free_game_imgs.c */
+void	free_wall_imgs(t_cub3D *game);
+void	free_compass_imgs(t_cub3D *game);
+void	free_minimap_imgs(t_cub3D *game);
+void	free_sprite_arr(t_cub3D *game);
+void	free_all_imgs(t_cub3D *game);
+
+/* free.c */
+void	free_array(char **str);
+void	free_img(void *mlx, void *ptr);
+void	free_sprites(void *mlx, t_image *sprite);
 void	ft_free(t_cub3D *game);
 
+/* game_init.c */
+void	init_game(t_cub3D *game);
+void	set_screen_size(t_cub3D *game);
+
+/* image.c */
+void	put_images_to_window(t_cub3D *game);
+void	get_pixel_colour(t_cub3D *game, t_image *img, int x, int y);
+void	draw_to_image(t_cub3D *game, int x, t_image *img);
+t_image	*set_image_ptr(t_cub3D *game, int side);
+void	pixel_put_image(t_cub3D *game, t_image *img, int x, int y);
+
+/* key_hook.c */
+void	toggle_shift(t_cub3D *game);
+void	toggle_tab(t_cub3D *game);
 int		ft_key(int keycode, t_cub3D *game);
 int		ft_mouse(t_cub3D *game);
 
-void	move_player(t_cub3D *game, double y, double x, int grad);
-void	rotate_player(t_cub3D *game, int dir);
-
-int		add_door(t_cub3D *game, int x, int y);
-int		check_door(t_cub3D *game, int side);
-int		assign_door(t_cub3D *game);
-void	get_door_colour(t_cub3D *game, t_image *img, int x, int y);
-void	find_door(t_cub3D *game);
-void	open_door(t_cub3D *game, int x, int y);
-int		find_door_node(t_cub3D *game, int new_x, int new_y);
-
-int		colourshift(int t, int r, int g, int b);
-void	raycast(t_cub3D *game);
-void	make_minimap(t_cub3D *game);
-void	refresh_minimap(t_cub3D *game);
-void	get_pixel_colour(t_cub3D *game, t_image *img, int x, int y);
+/* lines.c */
+void	draw_line(t_cub3D *game, t_line *line);
 void	line_algo(t_cub3D *game, int x, int y);
 
-void	init_ray(t_cub3D *game, int x);
-void	init_compass(t_cub3D *game);
-void	put_compass(t_cub3D *game, int x, int y);
-void	get_delta_dist(t_cub3D *game);
-void	get_side_dist(t_cub3D *game);
-void	get_perp_wall_dist(t_cub3D *game, int side);
-void	get_hit(t_cub3D *game, int *side);
-void	get_draw_coords(t_cub3D *game);
-void	get_tex_pos(t_cub3D *game, int side);
+/* minimap.c */
+void	make_minimap(t_cub3D *game);
+void	refresh_minimap(t_cub3D *game);
 
-t_image	*set_image_ptr(t_cub3D *game, int side);
-void	draw_to_image(t_cub3D *game, int x, t_image *img);
-void	put_images_to_window(t_cub3D *game);
-void	pixel_put_image(t_cub3D *game, t_image *img, int x, int y);
-
-void	player_start_direction(int x, int y, t_cub3D *game);
+/* mouse_move.c */
+void	rotate_left(t_cub3D *game, int x_dif);
+void	rotate_right(t_cub3D *game, int x_dif);
 int		mouse_move(int x, int y, t_cub3D *game);
 
-int	check_sprite(t_cub3D *game, int side);
-int	assign_sprite(t_cub3D *game);
-int	get_sprite_dist(t_cub3D *game);
-void	get_sprite_size(t_cub3D *game, t_spr_cast *info, int i);
-void	get_sprite_coords(t_cub3D *game, t_spr_cast *info);
-void	move_sprite(t_cub3D *game, int i);
+/* movement.c */
+void	rotate_player(t_cub3D *game, int dir);
+void	move_player(t_cub3D *game, double y, double x, int grad);
+
+/* player_direction.c */
+void	player_start_direction(int x, int y, t_cub3D *game);
+
+/* put_sprite.c */
+void	put_sprite_colour(t_cub3D *game, int tex_x, int x, int y);
+void	put_sprite(t_cub3D *game, t_spr_cast *info);
 int		draw_sprites(t_cub3D *game);
+
+/* raycast_calc_2.c */
+void	get_draw_coords(t_cub3D *game);
+void	get_tex_pos(t_cub3D *game, int side);
+void	put_compass(t_cub3D *game, int x, int y);
+void	init_compass(t_cub3D *game);
+
+/* raycast_calc.c */
+void	init_ray(t_cub3D *game, int x);
+void	get_delta_dist(t_cub3D *game);
+void	get_hit(t_cub3D *game, int *side);
+void	get_side_dist(t_cub3D *game);
+void	get_perp_wall_dist(t_cub3D *game, int side);
+
+/* raycast.c */
+void	make_minimap(t_cub3D *game);
+void	raycast(t_cub3D *game);
+int		render(t_cub3D *game);
+
+/* sprite_anim.c */
 void	scare_sprite(t_cub3D *game, int i);
 void	deactivate_scare(t_cub3D *game, int i);
 void	deactivate_sprite(t_cub3D *game, int i);
 void	activate_sprite(t_cub3D *game, int i);
 void	idle_sprite(t_cub3D *game, int i);
+
+/* sprite_cast.c */
+void	sort_sprites(int *sprite_order, \
+		int *sprite_distance, int total_sprites);
+int		get_sprite_dist(t_cub3D *game);
+void	get_sprite_coords(t_cub3D *game, t_spr_cast *info);
+void	get_sprite_size(t_cub3D *game, t_spr_cast *info, int i);
+
+/* sprite_move.c */
+
+void	move_sprite(t_cub3D *game, int i);
 void	inc_anims(t_cub3D *game);
 
-void	get_pixel_colour(t_cub3D *game, t_image *img, int x, int y);
-void	get_sprite_colour(t_cub3D *game, int x, int y);
-int	check_colour_pix(t_image *data, int x, int y);
-
-int	add_sprite(t_cub3D *game, int x, int y);
-
-time_t	get_time_in_ms(void);
-void	play_sound(char *str);
-
+/* sprite.c */
+int		add_sprite(t_cub3D *game, int x, int y);
 void	add_data_addr(t_image *img);
-int	make_sprite_array(t_cub3D *game);
+int		assign_sprite(t_cub3D *game);
+int		make_sprite_array(t_cub3D *game);
 
-void	free_array(char **str);
-void	free_img(void *mlx, void *ptr);
-void	free_sprites(void *mlx, t_image *sprite);
-
-void	free_wall_imgs(t_cub3D *game);
-void	free_compass_imgs(t_cub3D *game);
-void	free_minimap_imgs(t_cub3D *game);
-void	free_all_imgs(t_cub3D *game);
+/* utils.c */
+void	my_mlx_pixel_put(t_image *data, int x, int y, int color);
+void	ft_swap(int *distance_x, int *distance_y, int *order_x, int *order_y);
+double	ft_abs(double x);
+void	play_sound(char *str);
+time_t	get_time_in_ms(void);
 
 #endif
